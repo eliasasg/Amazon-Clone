9f04bcd20c2c66bf83a4929e980a9ef3ab51ca8f
Elias <eliasasg@gmail.com>
2024-05-13 19:21:27 -0500

    category and product

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import CarouselEffect from './component/Carousel/CarouselEffect';
+
 import Header from './component/Header/Header';
+import Product from './component/Product/Product';
+import Category from './component/catagory/Category';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <CarouselEffect />
+     <Category />
+     <Product />
+     
     </div>
   );
 }
