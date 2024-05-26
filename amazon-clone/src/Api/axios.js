bb4e8dbaf4d65165c3979e47610e1f410d572dd2
Elias <eliasasg@gmail.com>
2024-05-26 11:18:12 -0500

    deploying on netlify

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000"
+    // baseURL: "http://localhost:5000",
+    // amazon-api-deploy
+    baseURL: "https://project-two-api-deploy.onrender.com"
 })
 export {axiosInstance}
